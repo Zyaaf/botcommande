f7da1de7f5923fc4bc0d130b4fb8dd9315d26b81
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-06-20 18:47:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -203,7 +203,15 @@ message.channel.delete();
                     .then(function(list){
                         message.channel.bulkDelete(list);
                     }, function(err){message.channel.send("Erreur")})
-            message.channel.send('Vous avez réussie à vider le salon.')}
+                                                }
+       if(message.content === prefix + "clear"){
+           var embed = new Discord.RichEmbed()
+                 .setTitle("SALON VIDE")
+                 .setDescription("")
+                 .addField("Salon vidé","Salon vidé avec succes par un membre de l'équipe")
+                 .setColor("Ox09FE01")
+                        message.channel.sendEmbed(embed)
+       }
                     console.log("La commande clear viens d'être effectué par un membre de l'équipe.")
     }
 })
