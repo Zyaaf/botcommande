9bf9b3bee1fe5125ce7f881c14ca7b658fee81bf
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-06-24 12:00:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ bot.on('message', message => {
        
     if (message.content.startsWith(prefix + "sondage")) {
    if(message.author.id === "371914890903945216"){
+       message.delete()
        let args = message.content.split(" ").slice(1);
        let thingToEcho = args.join(" ")
        var embed = new Discord.RichEmbed()
