75e60743e6bce20e5b945fb9fc6d8d3c96574cbc
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-06-24 21:08:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -60,6 +60,44 @@ bot.on('message', message => {
         }
 });
 
+Bot.on("message", async message => {
+
+  if(message.content === prefix + "mute"){
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les droits pour muter un utilisateur !");
+
+    let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if(!toMute) return message.channel.send("Merci d'entrer un utilisateur !");
+    let role = message.guild.roles.find(r => r.name === "Utilisateurs mutés");
+    if(!role){
+      try {
+        role = await message.guild.createRole({
+          name: "Utilisateurs mutés",
+          color:"#000000",
+          permissions:[]
+        });
+
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(role, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      } catch (e) {
+        console.log(e.stack)
+      }
+    }
+
+    if(toMute.roles.has(role.id)) return message.channel.send('Cet utilisateur est déjà muté !');
+
+    await(toMute.addRole(role));
+    message.channel.send("Je l'ai muté !");
+
+    return;
+  }
+
+});
+
 bot.on('message', message => {
     let args = message.content.split(" ").slice(1);
 
