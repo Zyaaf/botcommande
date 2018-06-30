c7f16a687b5afb63feb7b8a89e92525a6bae5b2b
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-06-30 11:40:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -202,6 +202,17 @@ bot.on('message', message => {
             .setColor(0x5B4EB5)
         message.channel.sendEmbed(embednom)
     }
+    
+     if(message.content === prefix + "monprofil"){
+         var embednom = new Discord.RichEmbed()
+             .setTitle(`Voici le profil de ${message.author.username}`, true)
+             .setDescription("En dessous seront vos informations")
+             .setImage(`${message.author.avatar}`, true)
+             .addField("Voici votre id : ",`${message.author.id}`, true)
+             .setFooter("Votre profil")
+             .setColor(0x31BA4D)
+         message.channel.send(embednom)
+   }
 
      if(message.content === prefix + "monid"){
         var embednom = new Discord.RichEmbed()
