2793fdc37ca1d256ce4e5bede38da24d3b542f6f
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-06-30 23:00:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -52,6 +52,33 @@ bot.on('message', message => {
 });
 
 bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.length.split(/ +/);
+    command = args.shift().toLowerCase();
+
+    if (command === "Kick") {
+        let modRole = message.guild.roles.find("name", "Modérateur");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utilisateur à expulser").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+           return message.reply("Cet utilisateur est introuvable ou impossible à expulser.")
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
+            message.guild.channels.find("name", "general").send(`**${member.user.username} a été expulsé du discord par **${messaga.author.username}**`)
+            
+    }).catch(console.error)
+});
+
+bot.on('message', message => {
        
     if (message.content.startsWith(prefix + "vraioufaux")) {
    if(message.author.id === "371914890903945216"){
