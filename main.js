94252cce5ddbc65c346968947ce0e824af998529
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-08-30 21:13:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const discord = require("discord.js");
 const clientDiscord = new discord.Client();
 const PREFIX = "-";
 
+clientDiscord.on("ready", () => {
+  console.log("Prêt Chef");
+  clientDiscord.channels.get("484802599808401468").send("Prêt");
+});
+
 //Connexion
 clientDiscord.login(process.env.TOKEN);
 
