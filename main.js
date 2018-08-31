d608af1cf2a0dcef0db9f69e68317de3730c8208
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-09-01 00:26:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const discord = require("discord.js");
 const clientDiscord = new discord.Client();
 const PREFIX = "-";
 
+
 clientDiscord.on("ready", () => {
   clientDiscord.user.setGame('-help | StaffMe', 'https://www.twitch.tv/thomlorbrock');
   console.log("Prêt Chef");
@@ -10,7 +11,7 @@ clientDiscord.on("ready", () => {
 });
 
 //Connexion
-clientDiscord.login(process.env.TOKEN);
+clientDiscord.login("NDQ4MTk5NDc4NTQ2MTM3MDg4.Dhj4SA.GXaycy0gHiAfsnotocZaVUjcIpU");
 
 clientDiscord.on("message", message => {
   
@@ -20,9 +21,79 @@ clientDiscord.on("message", message => {
      }
   
   if(message.content === PREFIX + "contact"){
-     message.channel.send("Adresse Mail : thomlorbrok@gmaiL.com /n Pseudo Discord : Thom.Lorbrok#8058")
-     clientDiscord.channels.get("484802599808401468").send("Contact");
+    message.author.createDM().then(channel => {
+      channel.send("Adresse Mail : thomlorbrok@gmaiL.com /n Pseudo Discord : Thom.Lorbrok#8058")
+    });
+    clientDiscord.channels.get("484802599808401468").send("Contact");
   }
+  
+  if(message.content.startsWith(prefix + "ban")) {
+        if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send("Vous n'avez pas la perission");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez mentionner un utilisateur");
+        }
+
+        var ban = message.guild.member(message.mentions.users.first());
+        if(!ban) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour ban");
+        }
+        ban.ban().then(member => {
+            message.channel.send(`${member.user.username} est ban par ${message.author.username} !`)
+        });
+        
+    }
+
+	if(message.content.startsWith(prefix + "clear")) {
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.send("Vous n'avez pas la permission !");
+
+        let args = message.content.split(" ").slice(1);
+
+        if(!args[0]) return message.channel.send("Tu dois préciser un nombre de messages à supprimer !")
+        message.channel.bulkDelete(args[0]).then(() => {
+            message.channel.send(`${args[0]} messages ont été supprimés !`);
+        });
+    }
+	
+	if(message.content.startsWith(prefix + "mute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: false}).then(member => {
+            message.channel.send(`${mute.user.username} est mute !`);
+        });
+    }
+	
+	if(message.content.startsWith(prefix + "unmute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: true}).then(member => {
+            message.channel.send(`${mute.user.username} n'est plus mute !`);
+        });
+    }
   
 });
 
