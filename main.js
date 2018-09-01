e286fa4546e64b5fbe46748c70b7a4e3e9d67680
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-09-01 12:23:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 const discord = require("discord.js");
 
-const clientDiscord = new discord.Client();
+const bot = new discord.Client();
 const PREFIX = "-";
 
 
-clientDiscord.on("ready", () => {
-  clientDiscord.user.setGame(numbserv + ' SERVS ' + serv + ' USER', 'https://www.twitch.tv/thomlorbrock');
+bot.on("ready", () => {
+  bot.user.setActivity(numbserv + ' SERVS • ' + user + ' USERS :maple_leaf:', "https://www.twitch.tv/nekooo ")
   console.log("Prêt Chef");
-  clientDiscord.channels.get("484802599808401468").send("Prêt");
+  bot.channels.get("484802599808401468").send("Prêt");
 });
 
 //Connexion
-clientDiscord.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
 
-clientDiscord.on("message", message => {
+bot.on("message", message => {
   
   if(message.content === PREFIX + "idchannel"){
      if(message.channel.send(message.channel.id));
-     clientDiscord.channels.get("484802599808401468").send("ID channel utilisé !");
+     bot.channels.get("484802599808401468").send("ID channel utilisé !");
      }
   
   if(message.content === PREFIX + "contact"){
     message.author.createDM().then(channel => {
       channel.send("Adresse Mail : thomlorbrok@gmaiL.com /n Pseudo Discord : Thom.Lorbrok#8058")
     });
-    clientDiscord.channels.get("484802599808401468").send("Contact");
+    bot.channels.get("484802599808401468").send("Contact");
   }
 
 });
  
-clientDiscord.on("message", message => {
+bot.on("message", message => {
   if(message.content === PREFIX + "secret"){
     let role = message.guild.roles.find('name', 'Role Secret')
     
     if(message.member.roles.find('name', 'Role Secret')){
       message.member.removeRole(role)
       message.reply("Tu n'as plus le role !");
-      clientDiscord.channels.get("484802599808401468").send("Role enlevé !");
+      bot.channels.get("484802599808401468").send("Role enlevé !");
     }
     else {
       message.member.addRole(role)
       message.reply("Voilà le role !");
-      clientDiscord.channels.get("484802599808401468").send("Role atribué");
+      bot.channels.get("484802599808401468").send("Role atribué");
     }
   }
 });
